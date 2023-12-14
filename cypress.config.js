05651a60390f48aa795b62c09b14edf451d85481
jantunez <angelo.antunez@interseguro.com.pe>
2023-12-13 19:38:34 -0500

    testing

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {},
     type: "cucumber",
-    baseUrl: 'https://www.automationexercise.com',
+    baseUrl: 'https://www.automationexercise.com/',
     excludeSpecPattern: [],
     supportFile: false,
   },
